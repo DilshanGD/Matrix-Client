44d7d42a2d9022617b4d3676220903cdc66a48fc
Dilshan Upasena <98dilshanup@gmail.com>
2024-11-21 02:07:36 +0530

    Initial Frontend

---
+++ src/pages/student/Student.js
@@ -0,0 +1,25 @@
+// frontend/src/pages/student/Student.js
+
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import StudentTopbar from './components/StudentTopbar';
+import StudentSidebar from './components/StudentSidebar';
+import './css/Student.css'; // Import the CSS file for styling
+
+const Student = () => {
+  return (
+    <div className="student-page">
+      <StudentTopbar />
+      <div className="student-body">
+        <StudentSidebar />
+        <div className="page-content">
+          <Routes>
+            {/* Add your routes here */}
+          </Routes>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Student;
